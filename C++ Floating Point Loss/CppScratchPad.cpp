50ec540538ba416136ea3356f6bd0625107dfb5a
Kulpot <kulpotwahu@gmail.com>
2024-06-18 06:35:56 +0800

    ..

--- C++ Floating Point Loss/CppScratchPad.cpp
+++ C++ Floating Point Loss/CppScratchPad.cpp
@@ -1,3 +1,5 @@
-
+#include<iostream>
+using std::cout;
+using std::endl;
 
 //C++ Floating Point Loss   //ref link:https://www.youtube.com/watch?v=xiCPXMg7Tjk&list=PLRwVmtr-pp04ljVWgUFlQoBl9tqTzFh2N&index=6
